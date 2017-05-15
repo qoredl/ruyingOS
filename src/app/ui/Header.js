b6f85a2b35470c3d0b72b1f6a85499c89a01e8eb
ok111net <ok111net@163.com>
2017-05-15 15:08:22 +0800

    react-rui图标系统更换使用antd图标系统

--- src/app/ui/Header.js
+++ src/app/ui/Header.js
@@ -4,9 +4,9 @@
  */
 export default props => {
 	return (<header className={'r-header'}>
-		<div className={'r-header-l'}><a href="" className="r-icon r-icon-arrow">返回</a></div>
+		<div className={'r-header-l'}><a href="" className="r-icon r-icon-left">返回</a></div>
 		<h2 className={'r-header-title'}>{props.title}</h2>
-		<div className={'r-header-r'}><span className="r-icon r-icon-search">&nbsp;</span><span className="r-icon r-icon-renminbi">&nbsp;</span></div>
+		<div className={'r-header-r'}><span className="r-icon r-icon-user">&nbsp;</span><span className="r-icon r-icon-github">&nbsp;</span></div>
 	</header>);
 }
 
