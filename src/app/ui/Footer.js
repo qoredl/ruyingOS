79894475bc841a3a7115ea3ebdd5a1900f2ae409
ok111net <ok111net@163.com>
2017-05-16 12:30:14 +0800

    antd样式表备份

--- src/app/ui/Footer.js
+++ src/app/ui/Footer.js
@@ -2,12 +2,12 @@
  * app底部组件
  * date:2016-11-24
  */
-import { Menu, } from 'antd';
+import { Menu,Icon, } from 'antd';
 
 export default props => {
   return (<Menu className="r-footer nav">
-    <Menu.Item><a href="/#">首页</a></Menu.Item>
-    <Menu.Item><a href="/#user">me</a></Menu.Item>
+    <Menu.Item><a href="/#"><Icon type="left-circle-o" />首页</a></Menu.Item>
+    <Menu.Item><a href="/#user"><Icon type="aliwangwang" />me</a></Menu.Item>
     <Menu.Item><a href="http://ruanyf.github.io/es-checker/index.cn.html">es-checker</a></Menu.Item>
   </Menu>);
 }
