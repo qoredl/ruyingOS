f88c2ec652b2a14755c1d0547b56c987cc55e7a7
ok111net <ok111net@163.com>
2017-05-21 19:12:08 +0800

    增加webstrom配置文件

---
+++ cloud/beacon.js
@@ -0,0 +1,10 @@
+/**
+ * html5 beacon服务
+ * 本文件改动会比较频繁
+ * date:2017-3-25
+ */
+ const util = require('util');
+module.exports=function (req,res) {
+  console.log(req.url,req.body);
+  res.send('sldfj');
+};
--- src/app/Home/Index.js
+++ src/app/Home/Index.js
@@ -4,7 +4,7 @@
  */
 import { connect } from 'react-redux';
 import Footer from '../ui/Footer';
-import Htmlfiv from '../ui/Htmlfiv';
+import Html5 from '../ui/Html5';
 import '../ui/app.less';
 
 export default connect(({ homeState }) => ({
@@ -15,7 +15,7 @@ export default connect(({ homeState }) => ({
         <div className={'r-wrap'}>
           <h1 className={'bg-test'}>首页88886888888</h1>
           <img src={require('../images/logo.png')}/>
-          <Htmlfiv {...props}/>
+          <Html5 {...props}/>
         </div>
         <Footer/>
       </div>);
